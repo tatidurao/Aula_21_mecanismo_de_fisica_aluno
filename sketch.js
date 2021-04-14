3f9833d9187f7dd15b720d8ea3620603a3d1d8c7
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-04-14 15:33:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+ 
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
+}
+
